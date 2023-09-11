e5b77369cf89682a6a0623b85291c0591fd2279d
gastonluna <gaston.e.luna01@gmail.com>
2023-09-11 14:02:47 -0300

    Dudas con el 15 de los NaN,el 9 y el 5

--- Guia2.R
+++ Guia2.R
@@ -287,6 +287,30 @@ for (i in 1:length(numeros)) {
   cuadrado[i]<-numeros[i]**2
 }
 #Ejercicio 9??????----
+a<-as.numeric(readline("Ingrese el valor deseado: "))
+x=1 #Por que va el 1??????
+for (i in 1:1000) {
+  x<-(x-((x**2)-a)/(2*x))
+  if(x-(x-((x**2)-a)/(2*x)) <10**(-4)){ #Modificacion para el punto B
+    print("La diferencia entre Xn y Xn+1 es menor a 10 ^-4")
+    break
+  }
+}
+cat("La raiz cuadrada de",a,"es",x)
+
+#c
+i<-1
+x=1
+repeat{
+  x<-(x-((x**2)-a)/(2*x))
+  if(x-(x-((x**2)-a)/(2*x)) <10**(-4)){
+    print("La diferencia entre Xn y Xn+1 es menor a 10 ^-4")
+    break
+  }
+  i<-i+1
+  if(i>10000) break
+}
+cat("La raiz cuadrada de",a,"es",x)
 #Ejercicio 10----
 rm(list = ls())
 x<-as.numeric(readline("Ingrese el valor entre -pi y pi: "))
